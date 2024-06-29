e816f1d2baaa55f5d66cf539ac831512489b7c3c
~ck~ <120171593+C-K-Devs@users.noreply.github.com>
2024-06-29 10:22:10 +0530

    Update
    Ground layer adder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,23 +8,38 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
-    private bool isGrounded;
+    private Animator animator;
+    private bool isInverseWorld;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        isInverseWorld = SceneManager.GetActiveScene().name == "InverseWorld";
     }
 
     void Update()
     {
         // Movement
         float moveInput = Input.GetAxis("Horizontal");
+        if (isInverseWorld)
+        {
+            moveInput = -moveInput;
+        }
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Update animator parameters
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
 
         // Jumping
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            animator.SetBool("IsJumping", true);
+        }
+        else if (IsGrounded())
+        {
+            animator.SetBool("IsJumping", false);
         }
     }
 
