f54e8cdaaef0597139e605fdfef9c7e7f4dbdae0
~ck~ <120171593+C-K-Devs@users.noreply.github.com>
2024-06-30 15:48:39 +0530

    Updated
    Complete UI, lever fix done, scene switch done

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,11 +1,39 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor.UI;
 
 public class SceneChanger : MonoBehaviour
 {
+    public RectTransform headingUiElement; // Assign the UI element in the inspector
+    public Vector2 originalPosition;
+    public Vector2 headingTargetPosition;
+    public GameObject mainPanelShadow;
+    public GameObject info;
+
+    
+
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void DisplayInfo()
+    {
+        if (headingUiElement != null)
+        {
+            headingUiElement.anchoredPosition = headingTargetPosition;
+        }
+
+        mainPanelShadow.SetActive(false);
+        info.SetActive(true);
+
+    }
+
+    public void Back()
+    {
+        headingUiElement.anchoredPosition = originalPosition;
+        mainPanelShadow.SetActive(true);
+        info.SetActive(false);
     }
 
     public void QuitGame()
